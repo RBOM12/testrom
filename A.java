84879b8a1542381335ec0c40de57db9d6e98c3c2
RBOM12 <romainkiol@gmail.com>
2024-01-10 11:54:32 +0100

    Update A.java

--- A.java
+++ A.java
@@ -5,6 +5,10 @@ public abstract class A {
 	}
 	public abstract void m2();
 	
+	public void m4() {
+		System.out.println("A : m1()");
+	}
+	
 	public void m1() {
 		System.out.println("A : m1()");
 	}
